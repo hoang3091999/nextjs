33d0c66be0250f6d5260d1e107044ee7e272df8b
hoangle <hoangleminh30091999@gmail.com>
2023-01-03 08:45:55 +0700

    first commit

---
+++ pages/posts/first-post.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function Firstpost() {
+    return (
+        <>
+            <h1>Firstpost</h1>
+            <h2>
+                <Link href="/">Back to homepage</Link>
+            </h2>
+            
+        </>
+    )
+}
